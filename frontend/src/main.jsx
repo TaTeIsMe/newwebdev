21e2bb8c2145b475d564b2cef98a9da6fdb394ba
Mateusz <tatepon2@gmail.com>
2026-05-19 11:24:03 +0200

    ??????

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/tree_app'>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
